486b44cea5e3d59086ea04da7d9208d36f355e06
Selleen <roxypm10@gmail.com>
2023-06-12 17:17:03 -0400

    db

--- processes/database.py
+++ processes/database.py
@@ -1,17 +1,26 @@
 import sqlite3
 from sqlite3 import Error
+from parser_cran import get_data_from
+import re
+
+class Text:
+    def __init__(self, id, title, author, body):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.body = body
 
 class DataB:
     def __init__(self):
         self.connection = None #create_connection("./processes/sm_app.sqlite")
         
-    def insert_file(self, file_name):
-        create_users = f"""INSERT INTO
-        File(name)
+    def insert_file(self, text_file: Text):     
+        create_users = """INSERT INTO
+        File(Title, Author, Body)
         VALUES
-        ('{file_name}');
-        """        
-        self.execute_query(create_users)
+        (?, ?, ?);
+        """
+        self.execute_query(create_users, text_file)
         
     def close_connection(self):
         self.connection.close()
@@ -21,11 +30,13 @@ class DataB:
             self.connection = sqlite3.connect(path)
             create_files_table = '''
                 CREATE TABLE IF NOT EXISTS File (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Title TEXT NOT NULL UNIQUE,
+                    Author TEXT,
+                    Body TEXT NOT NULL
                 )
             '''
-            self.execute_query(create_files_table)
+            self.execute_query(create_files_table, "")
             print("Connection to SQLiteDB successful")
 
         except Error as e:
@@ -46,23 +57,57 @@ class DataB:
             print("ERROR EN EXECUTE READ QUERY")
             print(f"The error '{e}' occurred")
 
-    def execute_query(self, query):
+    def execute_query(self, query, text_file):
         print("ENtre aqui")
         cursor = self.connection.cursor()
         try:
-            cursor.execute(query)
+            if text_file != "":
+                cursor.execute(query, (text_file.title, text_file.author, text_file.body))
+            else: 
+                cursor.execute(query)
             self.connection.commit()
             print("Query executed successfully")
         except Error as e:
             print("ERROR EN EXECUTE QUERY")
-            print("The error '{e}' ocurred")
+            print(f"The error '{e}' ocurred")
+
+#Este metodo se llama inicialmente al levantar los servidores y asignar a cada uno su BD.
+# Aqui se toman los archivos .txt y se devuelven como objetos de la clase Text
+def convert_text_to_text_class(path, files_name: list):
+    text_list = []
+    
+    for file in files_name:
+        with open(f"{path}/{file}", 'r') as file:
+            data = file.read()
+        id = re.search(r'ID: (\d+)', data)
+        title = re.search(r'Title: ([\s\S]*?)\nAuthor: ', data, re.DOTALL)
+        author = re.search(r'Author: ([\s\S]*?)\nBody:', data, re.DOTALL )
+        body = re.search(r'Body:\n(.+)', data, re.DOTALL)
+        if id and title and author and body:
+            id_text = id.group(1).strip()
+            #title_text = title.group(1).strip()
+            title_ = title.group(1)
+            title_text = title_.replace('\n', ' ')
+            author_text = author.group(1).strip()
+            #body_text = body.group(1).strip()
+            body_ = body.group(1)
+            body_text = body_.replace('\n', ' ')
+            text = Text(id_text, title_text, author_text, body_text)
+            text_list.append(text)
+    return text_list
 
 if __name__ == '__main__':
-    path = './processes/databases/db_1.db'
+    path = './processes/databases/db3.db'
+    path_txts = '/home/roxy/Roxana-linux/SD/distributed_search_engine/processes/txts'
+    files_name = ['document_1.txt', 'document_2.txt', 'document_3.txt']
+    text_list = convert_text_to_text_class(path_txts, files_name)
     datab = DataB()
     datab.create_connection(path)
-    datab.insert_file("Hakuna Matata")
-    datab.insert_file("El viejo y el mar")
-    query = "Hakuna Matata"
-    select_files = f"SELECT name FROM File WHERE File.name = '{query}'"
-    result = datab.execute_read_query(select_files)
+    for text in text_list:
+        datab.insert_file(text)
+    
+    query = text_list[2].author
+    select_files = f"SELECT Author FROM File WHERE File.Author = '{query}'"
+    a = f"SELECT Author FROM File "
+    result = datab.execute_read_query(a)
+    print("RESULT ", result)
--- processes/parser_cran.py
+++ processes/parser_cran.py
@@ -1,12 +1,12 @@
 import re
 
-def create_txt_files(filename, path_save):
+def get_data_from(filename):
     with open(filename, 'r') as file:
         data = file.read()
         
     documents = re.split(r'\.I \d+', data)
     documents = [doc.strip() for doc in documents if doc.strip()]
-    
+    result = []
     for i, doc in enumerate(documents, start=1):
         doc_id_text = i
         title = re.search(r'\.T\n([\s\S]*?)\.A\n', doc, re.DOTALL)
@@ -17,17 +17,21 @@ def create_txt_files(filename, path_save):
             title_text = title.group(1).strip()
             author_text = author.group(1).strip()
             body_text = body.group(1).strip()
+            result.append((doc_id_text, title_text, author_text, body_text))
+    return result 
             
-            with open(f'{path_save}/document_{doc_id_text}.txt', 'w') as output_file:
-                output_file.write(f'ID: {doc_id_text}\n')
-                output_file.write(f'Title: {title_text}\n')
-                output_file.write(f'Author: {author_text}\n')
-                output_file.write(f'Body:\n{body_text}\n')
-                
-            print(f'Created document_{doc_id_text}.txt')
-        else:
-            print(f'Skipped document {i} due to missing data.')
+def create_txt_files(filename, path_save):
+    result = get_data_from(filename)
+    for doc in result:
+        with open(f'{path_save}/document_{doc[0]}.txt', 'w') as output_file:
+            output_file.write(f'ID: {doc[0]}\n')
+            output_file.write(f'Title: {doc[1]}\n')
+            output_file.write(f'Author: {doc[2]}\n')
+            output_file.write(f'Body:\n{doc[3]}\n')
+            print(f'Created document_{doc[0]}.txt')
+        
+    
 
-path_cran = '/home/roxy/Roxana-linux/SD/distributed_search_engine/processes/collections/cran/cran.all.1400'
-path_save = '/home/roxy/Roxana-linux/SD/distributed_search_engine/processes/txts'
-create_txt_files(path_cran, path_save)
+#path_cran = '/home/roxy/Roxana-linux/SD/distributed_search_engine/processes/collections/cran/cran.all.1400'
+#path_save = '/home/roxy/Roxana-linux/SD/distributed_search_engine/processes'
+#create_txt_files(path_cran, path_save)
